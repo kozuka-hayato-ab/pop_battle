35c4db1c3fdf6396690b67ac364684195f1cc7cc
伊東佑真 <giorno_haruno@yahoo.co.jp>
2018-09-28 21:00:59 +0900

    プレイヤーのアニメーションを調整

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -9,10 +9,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float cameraVerticalUpperLimit = 30f;
     private float cameraVerticalAngel;
 
-    [SerializeField, Range(1,4)] private int playerID;
+    [SerializeField, Range(1, 4)] private int playerID;
     private string mynameForInputmanager;
     private CharacterController characon;
     private Animator animcon;
+    [SerializeField] float m_RunCycleLegOffset = 0.2f; //specific to the character in sample assets, will need to be modified to work with others
+    const float halfNumber = 0.5f;
     private Vector3 playerMoveDirection = Vector3.zero;
 
     private const int maxHealth = 10;
@@ -30,7 +32,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float healthCureInterval;
     private bool healthCurePossible = true;
     private int healthCureValue = 2;//回復量
-    
+
     [SerializeField] GameObject myGun;
     [SerializeField] float bulletShotInterval;
     private bool bulletShotPossible = true;
@@ -44,33 +46,32 @@ public class PlayerController : MonoBehaviour
         mynameForInputmanager = "Gamepad" + playerID + "_";
     }
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         //視点操作　水平はプレイヤーの向きを変える　垂直はcamerapivotを回転
         transform.Rotate(new Vector3(0, Input.GetAxis(mynameForInputmanager + "CameraX") * playerLookSpeed * Time.deltaTime, 0), Space.Self);
         cameraVerticalAngel = Mathf.Clamp(cameraVerticalAngel + Input.GetAxis(mynameForInputmanager + "CameraY") * cameraAngleSpeed * Time.deltaTime,
             cameraVerticalUnderLimit, cameraVerticalUpperLimit);
         CameraPivot.transform.eulerAngles = new Vector3(cameraVerticalAngel, CameraPivot.transform.eulerAngles.y, CameraPivot.transform.eulerAngles.z);
-        
+
         //プレイヤー基準で移動方向を決める。
         var playerForward = Vector3.Scale(transform.forward, new Vector3(1, 0, 1)).normalized;
         float right = Input.GetAxis(mynameForInputmanager + "X");
         float forward = Input.GetAxis(mynameForInputmanager + "Y");
         Vector3 direction = playerForward * forward + transform.right.normalized * right;
 
-        //animcon.SetFloat("Right", right);
-        animcon.SetFloat("Forward", forward);
-
         if (characon.isGrounded)
         {
             playerMoveDirection.y = 0f;
             playerMoveDirection = direction * playerSpeedValue;
 
-            if(Input.GetButtonDown(mynameForInputmanager + "Jump"))
+            if (Input.GetButtonDown(mynameForInputmanager + "Jump"))
             {
                 playerMoveDirection.y = playerJumpValue;
             }
@@ -84,8 +85,9 @@ public class PlayerController : MonoBehaviour
             playerMoveDirection.y -= gravityStrength * Time.deltaTime;
         }
 
+        Debug.Log(playerMoveDirection.y);
 
-        if((Input.GetButton(mynameForInputmanager + "Shot2") || Input.GetKey(KeyCode.KeypadEnter)) && bulletShotPossible == true && bulletNumber > 0)
+        if ((Input.GetButton(mynameForInputmanager + "Shot2") || Input.GetKey(KeyCode.KeypadEnter)) && bulletShotPossible == true && bulletNumber > 0)
         {
             myGun.SendMessage("BulletShot");
             bulletNumber--;
@@ -93,7 +95,7 @@ public class PlayerController : MonoBehaviour
             StartCoroutine(WaitBulletShotInterval());
         }
 
-        if((Input.GetButton(mynameForInputmanager + "Shot1") || Input.GetKey(KeyCode.KeypadPlus)) && bombShotPossible == true && bombNumber > 0)
+        if ((Input.GetButton(mynameForInputmanager + "Shot1") || Input.GetKey(KeyCode.KeypadPlus)) && bombShotPossible == true && bombNumber > 0)
         {
             myGun.SendMessage("BombShot");
             bombNumber--;
@@ -110,6 +112,22 @@ public class PlayerController : MonoBehaviour
         }
 
         characon.Move(playerMoveDirection * Time.deltaTime);
+
+        if (!characon.isGrounded)
+        {
+            animcon.SetBool("OnGround", false);
+            animcon.SetFloat("Jump", playerMoveDirection.y);
+        }
+        else
+        {
+            animcon.SetBool("OnGround", true);
+            float runCycle = Mathf.Repeat(
+                animcon.GetCurrentAnimatorStateInfo(0).normalizedTime + m_RunCycleLegOffset, 1);
+            float jumpLeg = (runCycle < halfNumber ? 1 : -1) * forward;
+            animcon.SetFloat("JumpLeg", jumpLeg);
+        }
+        animcon.SetFloat("Forward", forward);
+        animcon.SetFloat("Right", right);
     }
 
     IEnumerator WaitBulletShotInterval()
@@ -132,7 +150,7 @@ public class PlayerController : MonoBehaviour
 
     public void HealthCure(int healthCureValue)
     {
-        if(playerHealth > 0 || playerHealth < 10)
+        if (playerHealth > 0 || playerHealth < 10)
         {
             playerHealth += healthCureValue;
             if (playerHealth > 10) playerHealth = 10;
@@ -161,7 +179,7 @@ public class PlayerController : MonoBehaviour
 
     public void ChangeGameController(int id)
     {
-        if(1 <= id && id <= 4) playerID = id;
+        if (1 <= id && id <= 4) playerID = id;
     }
 
 }

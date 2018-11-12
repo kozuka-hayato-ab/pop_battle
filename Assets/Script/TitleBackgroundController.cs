1433b651891625280ba7a54ef8214caf7fd6705c
小塚捷人 <>
2018-11-12 18:08:44 +0900

    araiさんの素材を適応した．

---
+++ Assets/Script/TitleBackgroundController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleBackgroundController : MonoBehaviour {
+	
+    [SerializeField] float scrollSpeed;
+    private RectTransform rt;
+    private void Start()
+    {
+        rt = GetComponent<RectTransform>();
+    }
+    private void FixedUpdate()
+    {
+        Vector2 pos = rt.anchoredPosition;
+        if (pos.x <= -800.0f)
+        {
+            pos.x = 800.0f;
+        }
+        pos.x -= scrollSpeed;
+        rt.anchoredPosition = pos;
+    }
+}
